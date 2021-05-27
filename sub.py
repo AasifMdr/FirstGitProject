031c89b6c57361908c0fd30220e46c6a332ca21f
Aasif Manandhar <aasifmanandhar09@gmail.com>
2021-05-27 14:08:30 +0545

    subtraction file added

---
+++ sub.py
@@ -0,0 +1,5 @@
+a = 50
+b = 40
+sub = a - b
+print(sub)
+print("The subtraction of two numbers is:",sub)
--- sum.py
+++ sum.py
@@ -2,3 +2,4 @@ a = 10
 b = 50
 sum = a + b
 print(sum)
+print('The sum of two numbers is:',sum)
